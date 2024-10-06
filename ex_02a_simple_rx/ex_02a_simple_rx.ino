98b4bf21aec92c5fae1fec8a4b6c87dfd0f0871f
dennis0324 <dennis0324@naver.com>
2024-10-07 01:40:04 +0900

    progress: success without dwt_configuretxrf

--- ex_02a_simple_rx/ex_02a_simple_rx.ino
+++ ex_02a_simple_rx/ex_02a_simple_rx.ino
@@ -2,25 +2,73 @@
 
 #define APP_NAME "SIMPLE RX v1.1"
 
+#define RNG_DELAY_MS 1000
+#define TX_ANT_DLY 16385
+#define RX_ANT_DLY 16385
+#define ALL_MSG_COMMON_LEN 10
+#define ALL_MSG_SN_IDX 2
+#define RESP_MSG_POLL_RX_TS_IDX 10
+#define RESP_MSG_RESP_TX_TS_IDX 14
+#define RESP_MSG_TS_LEN 4
+#define POLL_TX_TO_RESP_RX_DLY_UUS 240
+#define RESP_RX_TIMEOUT_UUS 400
+
+#define TX_ANT_DLY 16385
+#define RX_ANT_DLY 16385
+
+/* Frames used in the ranging process. See NOTE 3 below. */
+static uint8_t rx_poll_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, 'W', 'A', 'V', 'E', 0xE0, 0, 0};
+static uint8_t tx_resp_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, 'V', 'E', 'W', 'A', 0xE1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+/* Length of the common part of the message (up to and including the function code, see NOTE 3 below). */
+#define ALL_MSG_COMMON_LEN 10
+/* Index to access some of the fields in the frames involved in the process. */
+#define ALL_MSG_SN_IDX 2
+#define RESP_MSG_POLL_RX_TS_IDX 10
+#define RESP_MSG_RESP_TX_TS_IDX 14
+#define RESP_MSG_TS_LEN 4
+
+#define POLL_RX_TO_RESP_TX_DLY_UUS 2000
+
+/* Timestamps of frames transmission/reception. */
+static uint64_t poll_rx_ts;
+static uint64_t resp_tx_ts;
+
+#define IDLE_MODE 0x00
+
+/* Values for the PG_DELAY and TX_POWER registers reflect the bandwidth and power of the spectrum at the current
+ * temperature. These values can be calibrated prior to taking reference measurements. See NOTE 5 below. */
+// extern dwt_txconfig_t txconfig_options;
+
 // connection pins
 typedef struct {
   uint8_t rst; // reset
   uint8_t irq; // interupt
   uint8_t ss; // cs
+  dwt_txconfig_t txconfig;
 } uwb_data;
 
 uwb_data uwb_board[3] = {
   {
     .rst = 6,
     .irq = 7,
-    .ss = 5
+    .ss = 5,
+    .txconfig = {0x34,0xfdfdfdfd,0x0}
   },
   {
     .rst = 22,
     .irq = 24,
-    .ss = 26
-  },
+    .ss = 26,
+    .txconfig = {0x34,0xfdfdfdfd,0x0}
+  }
 };
+
+// uwb_board[0].txconfig.PGdly = 0x34;  /* PG delay. */
+// uwb_board[0].txconfig.power = 0xfdfdfdfd; /* TX power. */
+// uwb_board[0].txconfig.PGcount = 0x0;  /*PG count*/
+
+// uwb_board[1].txconfig.PGdly = 0x34;  /* PG delay. */
+// uwb_board[1].txconfig.power = 0xfdfdfdfd; /* TX power. */
+// uwb_board[1].txconfig.PGcount = 0x0;  /*PG count*/
 
 /* Default communication configuration. We use default non-STS DW mode. */
 static dwt_config_t config = {
@@ -55,39 +103,50 @@ void setup()
   /* Configure SPI rate, DW3000 supports up to 38 MHz */
   /* Reset DW IC */
   for(int i = 0; i < 2; i++){
+    Serial.print("check rc: ");
+    Serial.println(i);
+
     spiBegin(uwb_board[i].irq,uwb_board[i].rst);
     spiSelect(uwb_board[i].ss);
+
+    delay(200); // Time needed for DW3000 to start up (transition from INIT_RC to IDLE_RC, or could wait for SPIRDY event)
+
+    while (!dwt_checkidlerc()) // Need to make sure DW IC is in IDLE_RC before proceeding
+    {
+      UART_puts("IDLE FAILED\r\n");
+      while (1)
+        ;
+    }
+
+    dwt_softreset();
+    delay(200);
+
+    if (dwt_initialise(DWT_DW_INIT) == DWT_ERROR)
+    {
+      UART_puts("INIT FAILED\r\n");
+      while (1)
+        ;
+    }
+
+    // Enabling LEDs here for debug so that for each TX the D1 LED will flash on DW3000 red eval-shield boards.
+    dwt_setleds(DWT_LEDS_ENABLE | DWT_LEDS_INIT_BLINK);
+    // dwt_setlnapamode(DWT_LNA_ENABLE | DWT_PA_ENABLE); 
+
+    // Configure DW IC. See NOTE 5 below.
+    if (dwt_configure(&config)) // if the dwt_configure returns DWT_ERROR either the PLL or RX calibration has failed the host should reset the device
+    {
+      UART_puts("CONFIG FAILED\r\n");
+      while (1)
+        ;
+    }
+    // Serial.print(uwb_board[i].txconfig.PGcount);
+    // dwt_configuretxrf(&(uwb_board[i].txconfig));
+
+    dwt_setrxantennadelay(RX_ANT_DLY);
+    dwt_settxantennadelay(TX_ANT_DLY);
   }
 
-  delay(200); // Time needed for DW3000 to start up (transition from INIT_RC to IDLE_RC, or could wait for SPIRDY event)
-
-  while (!dwt_checkidlerc()) // Need to make sure DW IC is in IDLE_RC before proceeding
-  {
-    UART_puts("IDLE FAILED\r\n");
-    while (1)
-      ;
-  }
-
-  dwt_softreset();
-  delay(200);
-
-  if (dwt_initialise(DWT_DW_INIT) == DWT_ERROR)
-  {
-    UART_puts("INIT FAILED\r\n");
-    while (1)
-      ;
-  }
-
-  // Enabling LEDs here for debug so that for each TX the D1 LED will flash on DW3000 red eval-shield boards.
-  dwt_setleds(DWT_LEDS_ENABLE | DWT_LEDS_INIT_BLINK);
-
-  // Configure DW IC. See NOTE 5 below.
-  if (dwt_configure(&config)) // if the dwt_configure returns DWT_ERROR either the PLL or RX calibration has failed the host should reset the device
-  {
-    UART_puts("CONFIG FAILED\r\n");
-    while (1)
-      ;
-  }
+ 
   
 }
 
@@ -103,7 +162,7 @@ char a[10] = {0};
 void loop()
 {
   /* TESTING BREAKPOINT LOCATION #1 */
-  select_board(board_num);
+  // select_board(board_num);
 
   sprintf(a,"%d",board_num);
   test_run_info(a);
@@ -148,7 +207,7 @@ void loop()
     dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_ALL_RX_ERR);
   }
   unselect_board(board_num);
-  board_num = !board_num;
+  // board_num = !board_num;
 }
 /*****************************************************************************************************************************************************
  * NOTES:
